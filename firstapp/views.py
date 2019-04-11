d2ba5c9f203781714f103e93dbbe127984b1bfc8
Knik <d.suyash08@outlook.com>
2019-04-11 12:36:45 +0530

    second batch

--- firstapp/views.py
+++ firstapp/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from .models import Subjects
+from django.utils import timezone
 
 def post_list(request):
-    return render(request,'firstapp/post_list.html',{})
+    sub = Subjects.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request,'firstapp/post_list.html',{'sub':sub})
