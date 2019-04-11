d21bd6f705c284652fc19332cc0095ae693946bd
Knik <d.suyash08@outlook.com>
2019-04-11 16:27:04 +0530

    3rd submission

--- firstapp/views.py
+++ firstapp/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Subjects
 from django.utils import timezone
 
 def post_list(request):
     sub = Subjects.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request,'firstapp/post_list.html',{'sub':sub})
+
+def subjects_detail(request,pk):
+    sub = get_object_or_404(Subjects,pk=pk)
+    return render(request, 'firstapp/subjects_detail.html', {'sub':sub})
